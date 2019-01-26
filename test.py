ae96910bb56f9833ae516f1fd115f2b1cbacf48d
venomimm <venomimm@gmail.com>
2019-01-26 02:00:15 +0200

    Update test.py

---
+++ test.py
@@ -0,0 +1,13 @@
+from urllib.request import urlopen as uReq
+from bs4 import BeautifulSoup
+import requests
+
+result=requests.get("http://explorer.worldcryptoforumcoin.com/address/WcidA7rARPiczwhajfhuer7VDeywhAZgtt")
+
+print(result.status_code) 
+
+src=result.content
+soup = BeautifulSoup(src,"lxml")
+
+tag=soup.find_all("tr",{"role":"row"})
+print(tag)
