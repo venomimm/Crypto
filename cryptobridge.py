06d465c4597b334fe7cba43a002407f7787ecaf5
venomimm <venomimm@gmail.com>
2019-04-18 14:41:35 +0300

    18.04

--- cryptobridge.py
+++ cryptobridge.py
@@ -1,5 +1,3 @@
-from itertools import repeat
-from collections import defaultdict
 from bitshares import BitShares
 from bitshares.notify import Notify
 from bitshares.account import Account, AccountUpdate
@@ -11,29 +9,52 @@ from bitsharesbase.operations import getOperationNameForId
 from bitshares.blockchain import Blockchain, BlockchainInstance
 from win10toast import ToastNotifier
 from collections import OrderedDict
+from datetime import datetime
+import copy
 import math
 import os.path
 import ast
 import threading
+import multiprocessing
+from multiprocessing import Process, Queue
 import pandas as pd
-from datetime import datetime
 import queue
+import re
 
 
-class Account_Data:
+class Account_Data(threading.Thread):    
 
     def __init__(self, account):
+        self.order_data=pd
         self.account = Account(account, full=True)
+        self.notify = None
+        self.markets= [] #dict.fromkeys([Market.get_string(cat) for cat in self.account.openorders])
 
-        notify = Notify(
-            markets=list(dict.fromkeys([Market.get_string(cat)
-                                        for cat in self.account.openorders])),
-            accounts=[self.account.name],
-            on_account=self.on_acc
-            # on_market=self.on_mark,
-        )
 
-        # notify.listen()
+    def get_order(self):
+        return self.order_data
+
+    def set_order(self, date):      
+        self.order_data=date
+
+    def update_notify(self):   
+        if self.notify:
+            # Update the notification instance
+            self.notify.reset_subscriptions(list(self.account), list(self.markets))
+        else:
+            # Initialize the notification instance
+            self.notify = Notify(
+                markets=list(self.markets),
+                accounts=list(self.account),
+                on_market=self.on_mark,
+                on_account=self.on_acco
+                # on_block=self.on_block,
+                # bitshares_instance=self.bitshares
+            ) 
+
+    def run(self):
+        self.update_notify()
+        self.notify.listen()            
 
     def actual_tot(self, cat):
         ''' Transforms Amount in actual BTC value
@@ -123,53 +144,48 @@ class Account_Data:
                 for cat in self.account.history(only_ops=['fill_order', 'transfer', 'asset_issue']):
                     f.write(str(cat)+'\n')
 
-    def on_acc(self, account_update):
+    def on_acco(self, account_update):
         config_lock = threading.RLock()
         config_lock.acquire()
         account = account_update.account
         if account["name"] == self.account.name:
             id_tranz = account.blockchain.rpc.get_object(account.blockchain.rpc.get_object(
                 account_update['most_recent_op'])['operation_id'])
-            print(id_tranz['op'][0])
 
             if getOperationNameForId(id_tranz['op'][0]) in ['fill_order', 'transfer', 'asset_issue']:
                 toaster = ToastNotifier()
                 toaster.show_toast(getOperationNameForId(id_tranz['op'][0]).replace('_', ' ').title(),
                                    print(account_update),
                                    icon_path="bitshares.ico",
-                                   duration=5)
+                                   duration=10)
             self.write_hist()
-            print(self.my_bal())
+            print(id_tranz)
 
         config_lock.release()
 
     def avail(self):
         tot = [{'asset': cat['asset']['id'], 'amount':cat['amount'],
                 'symbol':cat['symbol']} for cat in self.account.balances+[cat['base'] for cat in self.account.openorders]]
-        return pd.DataFrame(tot, columns=['asset', 'symbol', 'amount']).groupby(['asset', 'symbol']).sum()
+        return pd.DataFrame(tot, columns=['asset', 'symbol', 'amount']).groupby(
+            ['asset', 'symbol']).sum()
 
-    def my_bal(self, val=0.000001):
+    def my_bal(self, val=0.0000001):
         tot = self.avail()
+        que = Queue()
 
-        que = queue.Queue()
-        threads_list = []
-
-        thred1 = threading.Thread(name='totalBTC', target=tot.insert, args=(0, 'total btc', [self.actual_tot(
+        thred1 = threading.Thread(name='totalBTC1', target=tot.insert, args=(0, 'total btc', [self.actual_tot(
             Amount(cat[1], cat[0][0]))['amount'] for cat in tot.iterrows()]))
-        thred2 = threading.Thread(name='availableBTC', target=lambda q, arg1: q.put(pd.DataFrame(arg1)), args=(que,
-                                                                                                               ({'asset': cat['asset']['id'], 'symbol': cat['symbol'], 'available BTC': self.actual_tot(cat)['amount']} for cat in self.account.balances)))
-        thred3 = threading.Thread(name='ordere', target=lambda q, arg1: q.put(pd.DataFrame(arg1)), args=(
+        thred2 = threading.Thread(name='availableBTC1', target=lambda q, arg1: q.put(pd.DataFrame(arg1)), args=(
+            que, ({'asset': cat['asset']['id'], 'symbol': cat['symbol'], 'available BTC': self.actual_tot(cat)['amount']} for cat in self.account.balances)))
+        thred3 = threading.Thread(name='ordere1', target=lambda q, arg1: q.put(pd.DataFrame(arg1)), args=(
             que, ({'asset': cat['asset']['id'], 'symbol': cat['symbol'], 'orders': cat['amount']} for cat in self.actual_sel())))
 
         thred1.start()
-        thred2.start()
         thred3.start()
-        threads_list.append(thred1)
-        threads_list.append(thred3)
-        threads_list.append(thred2)
-
-        for t in threads_list:
-            t.join()
+        thred2.start()
+        thred1.join()
+        thred3.join()
+        thred2.join()
 
         while not que.empty():
             result = que.get()
@@ -189,11 +205,35 @@ class Account_Data:
         return tot.sort_values('total', ascending=False)
 
     def on_mark(self, mark_update):
-        print(str(mark_update))
+        config_lock = threading.RLock()
+        config_lock.acquire()        
 
-    def get_assset_min(self, parit):
+        if mark_update.get("deleted", False):
+            self.order_data=self.order_data.drop(index=mark_update['id'], level=1)
+            print('Cancel '+str(mark_update))
+
+        if mark_update.get("for_sale", False):
+            if mark_update['sell_price']['base']['asset_id'] != Market(self.markets[0])['base']['id']:
+                dict_date={'ID':[mark_update['id']], 'Side':['Sell'],'Price': [mark_update.invert()['price']], mark_update['base']['symbol']:[mark_update['base']['amount']], mark_update['quote']['symbol']:[mark_update['quote']['amount']], 'Seller':[Account(mark_update['seller']).name]}
+            else:
+                dict_date={'ID':[mark_update['id']], 'Side':['Buy'],'Price': [mark_update['price']], mark_update['base']['symbol']:[mark_update['base']['amount']], mark_update['quote']['symbol']:[mark_update['quote']['amount']], 'Seller':[Account(mark_update['seller']).name]}
+            
+            if mark_update['id'] in self.order_data.index.get_level_values(1):
+                self.order_data.at[(dict_date['Side'][0], mark_update['id']), mark_update['base']['symbol']]= mark_update['for_sale']
+                self.order_data.at[(dict_date['Side'][0], mark_update['id']), mark_update['quote']['symbol']]=mark_update['for_sale']/mark_update['price']
+                
+                print('Update ID '+mark_update['id']+' '+Account(mark_update['seller']).name+' '+str(mark_update))
+            else:
+                self.order_data = pd.concat([self.order_data, pd.DataFrame(dict_date).set_index(['Side','ID'])], sort=True)
+                print('Add/remove ID '+mark_update['id']+' '+Account(mark_update['seller']).name+' '+str(mark_update))
+                
+            self.order_data.sort_values(by=['Price'], inplace=True)
+
+        config_lock.release()
+
+    def get_assset_min(self, parit, acc='venom88'):
         if isinstance(parit, str):
-            if 'BRIDGE.BTC' in parit.split(':')[1]:
+            if 'BRIDGE.BTC' in re.split(':|/', parit)[1]:
                 parit = Market(str(Market(parit).get_string().split(
                     ':')[1]+':'+Market(parit).get_string().split(':')[0]))
             else:
@@ -202,11 +242,11 @@ class Account_Data:
         am_quote = Amount(0, parit['quote']['id'])
         am_base = Amount(0, parit['base']['id'])
 
-        if os.path.isfile(self.account.name+'.log'):
+        if os.path.isfile(Account(acc).name+'.log'):
             self.write_hist()
             tot = self.read_history()[::-1]
         else:
-            tot = self.account.history(
+            tot = Account(acc).history(
                 only_ops=['fill_order', 'transfer', 'asset_issue'])
 
         for cat in tot:
@@ -229,7 +269,7 @@ class Account_Data:
             # if getOperationNameForId(cat['op'][0]) in ['asset_issue'] and cat['op'][1]['asset_to_issue']['asset_id'] in str(parit['base']):
             #     am_base += Amount(cat['op'][1]['asset_to_issue'])
 
-        return [Price(am_base, am_quote), am_base, am_quote]
+        return [Price(quote=am_base, base=am_quote), am_base, am_quote]
 
     def get_hist(self):
         hist = []
@@ -246,61 +286,100 @@ class Account_Data:
 
         return pd.DataFrame(hist)[['Transaction ID', 'Transaction type', 'Order ID', 'Pays', 'Receives']]
 
-    def market_ordere(self, market, limit=100):
+    def market_ordere(self, market, limit=300):
         if isinstance(market, str):
-            market = Market(market)
-
-        bids, asks = [], []
-
-        for cat in self.account.blockchain.rpc.get_limit_orders(market['base']['id'], market['quote']['id'], limit):
-            order = Order(cat['id'])
-            if order['base']['asset']['id'] == market['base']['id']:
-                bids.append(Order(cat['id']))
+            if 'BRIDGE.BTC' in re.split(':|/', market)[1]:
+                market = Market(str(Market(market).get_string().split(
+                    ':')[1]+':'+Market(market).get_string().split(':')[0]))
             else:
-                asks.append(Order(cat['id']))
+                market = Market(market)
 
-        return {"bids": bids, "asks": asks}
+        order_book = [] 
+        orders = self.account.blockchain.rpc.get_limit_orders(market['base']['id'], market['quote']['id'], limit)
+        orders = [self.get_updated_limit_order(o) for o in orders]
+        orders = [Order(o) for o in orders]
 
-    def mark_order_tot(self, list_ord=[]):
-        quote_sum = Amount(0, list_ord[0]['quote']['asset']['id'])
-        base_sum = Amount(0, list_ord[0]['base']['asset']['id'])
-        for cat in list_ord:
-            quote_sum += cat['quote']
-            base_sum += cat['base']
+        for order in orders:
+            if 'for_sale' in order:
+                if order['base']['asset']['id'] == market['base']['id']:
+                    order_book.append({'ID':order['id'], 'Side':'Sell','Price': order.invert()['price'], order['base']['symbol']:order['base']['amount'], order['quote']['symbol']:order['quote']['amount'], 'Seller':Account(order['seller']).name})
+                else:
+                    order_book.append({'ID':order['id'], 'Side':'Buy','Price': order['price'], order['base']['symbol']:order['base']['amount'], order['quote']['symbol']:order['quote']['amount'], 'Seller':Account(order['seller']).name})
 
-        return (quote_sum, base_sum)
+        return order_book
 
-    def truncate(self, number, decimals):
+    @staticmethod
+    def get_updated_limit_order(limit_order):
+        """ Returns a modified limit_order so that when passed to Order class,
+            will return an Order object with updated amount values
+            :param limit_order: an item of Account['limit_orders'] or bitshares.rpc.get_limit_orders()
+            :return: Order
+        """
+        order = copy.deepcopy(limit_order)
+        order = limit_order
+        price = float(order['sell_price']['base']['amount']) / float(order['sell_price']['quote']['amount'])
+        base_amount = float(order['for_sale'])
+        quote_amount = base_amount / price
+        order['sell_price']['base']['amount'] = base_amount
+        order['sell_price']['quote']['amount'] = quote_amount
+        return order        
+    
+    @staticmethod
+    def truncate(number, decimals):
         """ Change the decimal point of a number without rounding
             :param strig: A number to be cut down
             :param int | decimals: Number of decimals to be left to the float number
             :return: Price with specified precision
         """
-        return float(number[:decimals]+'.'+number[6:-1])
+        return float(number[:decimals]+'.'+number[decimals:-1])
 
+    def get_market_data(self, mark):
+        pd.options.display.float_format = '{:,.8f}'.format
+        date = self.market_ordere(mark)
+        order_book = pd.DataFrame(date).set_index(['Side','ID'])
+        order_book.sort_values(by=['Price'], inplace=True)
 
-# test = market_ordere('venom88', 'BRIDGE.PHON:BRIDGE.BTC')
+        return self.set_order(order_book)
 
-# print(mark_order_tot(test['bids']))
-# print(mark_order_tot(test['asks']))
-# print(truncate(Asset('BRIDGE.PHON',full=True)['dynamic_asset_data']['current_supply'], Asset('BRIDGE.PHON',full=True)['precision']))
+    def process_pd (self, panda):
 
-# list_asks = pd.DataFrame( OrderedDict({'Order ID': cat['id'], 'Price':str(Price(cat['price'],base=cat['quote'],quote=cat['base']))[:18], cat['base']['symbol']:cat['base']['amount'], cat['quote']['symbol']:cat['quote']['amount'], 'Seller':Account(cat['seller'])['name']}) for cat in test['asks'])
-# print(list_asks)
+        # sell = panda.loc['Sell']
+        # buy = panda.loc['Buy']
+        # buy=buy.sort_values(by=['Price'], ascending=False)
+        
+        # sell=sell.assign(Total=pd.Series(sell['BRIDGE.BTC'].rolling(window=sell['BRIDGE.BTC'].count(), min_periods=1).sum()))
+        # buy=buy.assign(Total=pd.Series(buy['BRIDGE.BTC'].rolling(window=buy['BRIDGE.BTC'].count(), min_periods=1).sum()))
+        panda=panda.assign(Total=pd.Series(panda['BRIDGE.BTC'].rolling(window=panda['BRIDGE.BTC'].count(), min_periods=1).sum()))
+        panda=panda.assign(Place=panda.loc[:,['BRIDGE.BTC']].apply(lambda x: x > panda['BRIDGE.BTC'].std()))    
+        # sell=sell.assign(Place=sell.loc[:,['BRIDGE.BTC']].apply(lambda x: x > sell['BRIDGE.BTC'].std()))
+        # buy=buy.assign(Place=buy.loc[:,['BRIDGE.BTC']].apply(lambda x: x > buy['BRIDGE.BTC'].std()))
 
-# lista_ord = []
-# for cat in account.openorders:
-#     if cat.market not in lista_ord:
-#         lista_ord.append(cat.market)
-#         print(get_assset_min(cat.market))
+        # sell['Percent Tot']=sell.loc[:,['BRIDGE.BTC']].apply(lambda x: x / x.sum())
+        # buy['Percent Tot']=buy.loc[:,['BRIDGE.BTC']].apply(lambda x: x / x.sum())
+        # sell['Percent Price']=sell.loc[:,['Price']].apply(lambda x: (x.shift(-1)-x)/x)
+        # buy['Percent Price']=buy.loc[:,['Price']].apply(lambda x: (x.shift(-1)-x)/x)
 
-
-accont = Account_Data('venom88')
-# print(accont.get_assset_min('BRIDGE.ZNY:BRIDGE.BTC'))
-
+        # print(buy)
+        # print(sell)
+        print(panda)
 
 start_time = datetime.now()
-print(accont.my_bal())
+
+accont = Account_Data('venom88')
+accont.markets=['BRIDGE.MOBI/BRIDGE.BTC']
+accont.get_market_data(accont.markets[0])
+accont.process_pd(accont.get_order())
+accont.get_assset_min('BRIDGE.PRJ/BRIDGE.BTC', 'venom88')
+
+accont.run()
+
+
+# # for i in range(5):
+# #     order = accont.market_ordere(markk)['bids'][i].invert()
+# #     sel = order['seller']
+# #     print(Account(sel).name)
+# #     print(accont.get_assset_min(markk, sel))
+# print(accont.get_assset_min(markk, 'venom88'))
 
 time_elapsed = datetime.now() - start_time
 print('Time elapsed (hh:mm:ss.ms) {}'.format(time_elapsed))
--- frontEnd.py
+++ frontEnd.py
@@ -1,33 +1,135 @@
-from tkinter import Button, Entry, Tk, StringVar, Text, END, Listbox
+from tkinter import ttk, NO, Button, Entry, Tk, RIGHT, LEFT, StringVar, Text, END, Listbox, Frame, Label, RIDGE, E, X
+from datetime import datetime
 from cryptobridge import Account_Data
+from collections import OrderedDict
+from bitshares.price import Price
+from bitshares.account import Account
+from bitshares.asset import Asset
+import multiprocessing
+import queue
+import threading
+import pandas as pd
 
 window = Tk()
-
-
-def get_data():
-    accont = Account_Data(val.get())
-    return accont
-
-
-def action_button():
-    t1.insert(END, get_data().my_bal())
-    t2.insert(END, get_data().my_orders()['sell'])
-
-
+window.configure(background='black')
 w, h = window.winfo_screenwidth(), window.winfo_screenheight()
 window.geometry("%dx%d+0+0" % (w, h))
 
-b1 = Button(window, text='Ok', command=lambda: action_button())
-b1.grid(row=0, column=1)
+frame1 = Frame(window, relief=RIDGE)
+frame1.grid(row=0, rowspan=5, column=0)
+frame2 = Frame(window, relief=RIDGE)
+frame2.grid(column=1, row=0)
+
+
+def get_data():
+    return Account_Data(val.get())
+
+
+def insert_my_orders():
+    if t2.size() > 0:
+        t2.delete(0, END)
+
+    for cat in get_data().my_orders()['buy']:
+        t2.insert(END, cat)
+        t2.itemconfig(END, fg='green')
+    for cat in get_data().my_orders()['sell']:
+        t2.insert(END, cat)
+        t2.itemconfig(END, fg='red')
+
+
+def insert_asset_orders(**kwargs):
+
+    t4.heading(1, text='Order ID')
+    t4.heading(2, text='Price')
+    t4.heading(3, text=kwargs['bids'][0]['base']['symbol'])
+    t4.heading(4, text=kwargs['bids'][0]['quote']['symbol'])
+    t4.heading(5, text='Seller')
+
+    for cat in kwargs['bids']:
+        t4.insert('', END, value=(cat['id'], str(cat.invert()).split('@')[1].split(' ')[
+                  1], cat['base']['amount'], cat['quote']['amount'], Account(cat['seller'])['name']))
+
+    t3.heading(1, text='Order ID')
+    t3.heading(2, text='Price')
+    t3.heading(3, text=kwargs['asks'][0]['base']['symbol'])
+    t3.heading(4, text=kwargs['asks'][0]['quote']['symbol'])
+    t3.heading(5, text='Seller')
+
+    for cat in kwargs['asks']:
+        t3.insert('', END, value=(cat['id'], str(cat).split('@')[1].split(' ')[1],
+                                  cat['base']['amount'], cat['quote']['amount'], Account(cat['seller'])['name']))
+
+
+def insert_my_balance():
+    t1.delete(1.0, END)
+    t1.insert(END, get_data().my_bal())
+
+
+def insert_log():
+    pass
+    # get_data().notify.listen()
+    # t3.insert(END, get_data().on_acc)
+
+
+def action_button():
+    p1 = threading.Thread(name='balanta', target=insert_my_balance)
+    p2 = threading.Thread(name='ordere', target=insert_my_orders)
+    # p3 = threading.Thread(name='log', target=insert_log)
+    p1.start()
+    p2.start()
+    # p3.start()
+
+
+def onselect_list(evt):
+    w = evt.widget
+    index = w.curselection()
+    value = w.get(index)
+    market = value.split('@')[1].split(' ')[2]
+    # result = get_data().market_ordere(market)
+    # insert_asset_orders(bids=result['bids'], asks=result['asks'])
+
+    val_min = get_data().get_assset_min(market)
+    textmin.set('Price:'+str(val_min[0])+'  Amount:' +
+                str(val_min[1])+'  Paid:'+str(val_min[2])+'  Current supply:'+str(get_data().truncate(Asset('BRIDGE.PHON', full=True)['dynamic_asset_data']['current_supply'], Asset('BRIDGE.PHON', full=True)['precision'])))
+
+
+labelAcont = Label(frame1, text='Account',
+                   relief=RIDGE, padx=20, pady=5)
+labelAcont.grid(row=0, column=0)
+labelBuy = Label(frame2, text='Buy orders',
+                 relief=RIDGE, padx=50, pady=5)
+labelBuy.grid(row=2, column=0)
+labelSell = Label(frame2, text='Sell orders',
+                  relief=RIDGE, padx=50, pady=5)
+labelSell.grid(row=2, column=1)
+
+textmin = StringVar()
+labelMin = Label(frame2, padx=10, pady=5, textvariable=textmin)
+labelMin.grid(row=0, columnspan=3)
 
 val = StringVar()
-entr = Entry(window, textvariable=val)
-entr.grid(row=0, column=0)
+entr = Entry(master=frame1, textvariable=val, width=35)
+entr.grid(row=0, column=1)
+entr.bind('<Return>', lambda event: action_button())
 
-t1 = Text(window, height=15, width=80)
-t1.grid(row=2, column=0)
+b1 = Button(frame1, text='Ok', command=lambda: action_button(), width=10)
+b1.grid(row=0, column=2)
+t1 = Text(frame1, height=15, width=80)
+t1.grid(row=1, columnspan=3)
+t2 = Listbox(frame1, height=15, width=100)
+t2.grid(row=2, columnspan=3)
+t2.bind('<<ListboxSelect>>', onselect_list)
 
-t2 = Listbox(window, height=10, width=80)
-t2.grid(row=1, column=0)
+t3 = ttk.Treeview(frame2, columns=(1, 2, 3, 4, 5), height=15)
+t3['show'] = 'headings'
+t3.grid(row=3, column=0)
+
+t4 = ttk.Treeview(frame2, columns=(1, 2, 3, 4, 5), height=15)
+t4['show'] = 'headings'
+t4.grid(row=3, column=1)
+
+for i in (1, 2, 3, 4, 5):
+    t4.column(i, minwidth=0, width=120, stretch=NO)
+    t3.column(i, minwidth=0, width=120, stretch=NO)
 
 window.mainloop()
